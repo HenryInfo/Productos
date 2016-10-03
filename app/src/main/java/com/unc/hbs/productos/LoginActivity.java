4b3cd30bd299d0a27f3340d3f7652646de0f7170
HenryInfo <hbravos@unc.edu.pe>
2016-10-03 07:58:28 -0500

    Register or login user

--- app/src/main/java/com/unc/hbs/productos/LoginActivity.java
+++ app/src/main/java/com/unc/hbs/productos/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -46,15 +47,12 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
     View focusView = null;
     Context c;
+    TextInputLayout textInputLayout;
+    boolean banderaNew=false;
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
@@ -68,7 +66,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private EditText mNewNombre;
 
+    private Button buttonCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,6 +79,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         c=this;
 
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        buttonCancel = (Button) findViewById(R.id.cancel);
+        buttonCancel.setEnabled(false);
+        buttonCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                banderaNew=false;
+                buttonCancel.setEnabled(false);
+                textInputLayout.setVisibility(View.INVISIBLE);
+            }
+        });
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -92,7 +102,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
         });
-
+        textInputLayout= (TextInputLayout) findViewById(R.id.Input);
+        mNewNombre =(EditText) findViewById(R.id.nombre);
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -169,75 +180,94 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         boolean cancel = false;
 
+        if(!banderaNew)
+        {
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
+            // Check for a valid password, if the user entered one.
+            if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+                mPasswordView.setError(getString(R.string.error_invalid_password));
+                focusView = mPasswordView;
+                cancel = true;
+            }
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(password)) {
-            mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-        //DBASE
+            // Check for a valid email address.
+            if (TextUtils.isEmpty(email)) {
+                mEmailView.setError(getString(R.string.error_field_required));
+                focusView = mEmailView;
+                cancel = true;
+            } else if (!isEmailValid(email)) {
+                mEmailView.setError(getString(R.string.error_invalid_email));
+                focusView = mEmailView;
+                cancel = true;
+            }
+            // Check for a valid email address.
+            if (TextUtils.isEmpty(password)) {
+                mPasswordView.setError(getString(R.string.error_field_required));
+                focusView = mPasswordView;
+                cancel = true;
+            } else if (!isEmailValid(email)) {
+                mPasswordView.setError(getString(R.string.error_invalid_password));
+                focusView = mPasswordView;
+                cancel = true;
+            }
+            //DBASE
 
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            boolean bandera=false;
+            if (cancel) {
+                // There was an error; don't attempt login and focus the first
+                // form field with an error.
+                focusView.requestFocus();
+            } else {
+                // Show a progress spinner, and kick off a background task to
+                // perform the user login attempt.
+                boolean bandera=false;
+                final DBAdapter db = new DBAdapter(this);
+                db.open();
+                //db.insertUser(new Usuario("henry@unc.edu.pe","12345",true));
+                Cursor c=db.getUserUser(mEmailView.getText().toString());
+                while(c.moveToNext()){
+                    String pass = c.getString(c.getColumnIndex(DBAdapter.PASS));
+                    if(pass.equals(DBAdapter.getMD5(mPasswordView.getText().toString())))
+                    {
+                        Toast.makeText(this, "Ingresando...", Toast.LENGTH_LONG).show();
+                        bandera=true;
+                        db.close();
+
+                    }
+                }
+
+                if(bandera)
+                {
+
+                    showProgress(true);
+                    mAuthTask = new UserLoginTask(email, password);
+                    mAuthTask.execute((Void) null);
+                }
+                else
+                {
+                    textInputLayout.setVisibility(View.VISIBLE);
+                    buttonCancel.setEnabled(true);
+                    focusView=mNewNombre;
+                    Toast.makeText(this, "Nuevo registro", Toast.LENGTH_LONG).show();
+                    focusView.requestFocus();
+                    banderaNew=true;
+                    //  focusView=mPasswordView;
+                    // focusView.requestFocus();
+
+                }
+
+            }
+        }
+        else
+        {
+
             final DBAdapter db = new DBAdapter(this);
             db.open();
-            //db.insertUser(new Usuario("henry@unc.edu.pe","12345",true));
-            Cursor c=db.getUserUser(mEmailView.getText().toString());
-            while(c.moveToNext()){
-                String pass = c.getString(c.getColumnIndex(DBAdapter.PASS));
-                if(pass.equals(DBAdapter.getMD5(mPasswordView.getText().toString())))
-                {
-                    Toast.makeText(this, "Ingresando...", Toast.LENGTH_LONG).show();
-                    bandera=true;
-                }
-            }
-
-            if(bandera)
-            {
-                db.close();
-
-                showProgress(true);
-                mAuthTask = new UserLoginTask(email, password);
-                mAuthTask.execute((Void) null);
-            }
-            else
-            {
-                focusView=mEmailView;
-                Toast.makeText(this, "Usuario o Password incorrectos", Toast.LENGTH_LONG).show();
-                focusView.requestFocus();
-                focusView=mPasswordView;
-                focusView.requestFocus();
-
-            }
-
+            db.insertUser(new Usuario(mEmailView.getText().toString(),mPasswordView.getText().toString(),true));
+            db.close();
+            Toast.makeText(this, "Registro exitosamente", Toast.LENGTH_LONG).show();
+            showProgress(true);
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
         }
     }
 
